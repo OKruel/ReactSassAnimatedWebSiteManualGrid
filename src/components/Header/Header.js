5dbe8d632d33fade5c9e76239277b8024ecffb74
olavokruel <olavokruel@gmail.com>
2019-10-01 17:42:20 -0300

    node-sass and root font-size

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,19 @@
 import React from 'react'
-import './Header.css'
+import './Header.scss'
 
 const Header = props => (
     <header className='header'>
-        <div className='logo-box'>
-            <img src='../../assets/img/logo-white.png' alt='Logo_Image' className='logo'></img>
+        <div className='header__logo-box'>
+            <img src='../../assets/img/logo-white.png' alt='Logo_Image' className='header__logo'></img>
         </div>
 
-        <div className='text-box'>
+        <div className='header__text-box'>
             <h1 className='heading-primary'>
-                <span className='heading-primary-main'>Outdoors</span>
-                <span className='heading-primary-sub'>is where life happens</span>
+                <span className='heading-primary--main'>Outdoors</span>
+                <span className='heading-primary--sub'>is where life happens</span>
             </h1>
 
-            <a href='#' className='btn btn-white btn-animated'>Discover our tours</a>
+            <a href='#' className='btn btn--white btn--animated'>Discover our tours</a>
         </div>
     </header>
 )
