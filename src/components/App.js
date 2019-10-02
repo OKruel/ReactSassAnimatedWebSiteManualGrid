c8e604efce08df4cccc2b71f4c2245f4db45a17f
olavokruel <olavokruel@gmail.com>
2019-10-02 20:08:40 -0300

    finished manual Grid

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import Header from '../components/Header/Header'
+// import Grid from '../components/GridManually/Grid'
 
 
 function App() {
   return (
     <div className='App' > 
       <Header></Header>
+      {/* <Grid></Grid> */}
     </div>
   );
 }
