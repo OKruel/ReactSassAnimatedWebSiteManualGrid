665cc8055a32366a9a5c2c8ddd9dffdfd53555d0
olavokruel <olavokruel@gmail.com>
2019-10-03 19:47:47 -0300

    building card heading

--- src/components/Section/SectionTours.js
+++ src/components/Section/SectionTours.js
@@ -15,8 +15,14 @@ const SectionTours = props => (
                         <div className='card__picture card__picture--1'>
                             &nbsp;
                         </div>
-                        <div className='card__heading'>Heading</div>
-                        <div className='card__details'>Details</div>
+                        <h4 className='card__heading'>
+                            <span className='card__heading--span card__heading--span-1'>
+                                The sea explorer
+                            </span>                            
+                        </h4>
+                        <div className='card__details'>
+                            Details
+                        </div>
                     </div>
                     <div className='card__side card__side--back card__side--back-1'>
                         Back
