3b89739e4214c6d3e569ec79a12c41daf7846377
olavokruel <olavokruel@gmail.com>
2019-10-11 03:14:35 -0300

    Minor bugs fixed. Ready for production

--- src/components/PopUp/PopUp.js
+++ src/components/PopUp/PopUp.js
@@ -11,8 +11,8 @@ const PopUp = props => (
             </div>
             <div className='popup__right'>
                 <a href='#section-tours' className='popup__close'>&times;</a>
-                <h2 className='heading-secondary u-margin-bottom-small'>Start booking now!</h2>
-                <h3 className='heading-tertiary u-margin-bottom-small'>Important &ndash; Please read this terms before booking</h3>
+                <h2 className='heading-secondary u-margin-bottom-small popup__head'>Start booking now!</h2>
+                <h3 className='heading-tertiary u-margin-bottom-small popup__head'>Important &ndash; Please read this terms before booking</h3>
                 <p className='popup__text'>Integer a tortor dolor. Etiam viverra lorem mauris, eget ullamcorper erat sodales a. Nunc mi felis, mollis nec ante eget, ultricies aliquet purus. Duis ut justo et risus porta posuere. Mauris vel sapien nec ligula commodo lobortis at eget sapien. Suspendisse sodales nec tortor vel interdum. Quisque et facilisis dui, eget sagittis mauris. Sed mollis non dolor ac hendrerit.</p>
                 <a href='#' className='btn btn--green'>Book now</a>
             </div>
